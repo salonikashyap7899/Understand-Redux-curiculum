48eeab56c7cfd6501204da87070f232f5684504f
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2025-06-21 20:42:10 +0530

    Add Remove and Add Itmem

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
 import { createStore } from 'redux'
 
+import { productsList } from './productsList.js'
+
+
 const initialState = {
-  post: 0,
-  name: 'Anurag Singh',
-  age: 26,
+  products: productsList,
+  cartItems :[],
+  wishList: [],
 }
 
-const INCREMENT = 'post/increment'
-const DECREMENT = 'post/decrement'
-const INCREASE_BY = 'post/increaseBy'
-const DECREASE_BY = 'post/decreaseBy'
+const CART_ADD_ITEM = 'cart/addItem';
+const CART_REMOVE_ITEM = 'cart/removeItem';
+const CART_ITEM_INCREASE_QUANTITY = 'cart/increaseItemQuantity';
+const CART_ITEM_DECREASE_QUANTITY = 'cart/decreaseItemQuantity';
+
+const WISHLIST_ADD_ITEM = 'cart/addItem';
+const WISHLIST_REMOVE_ITEM = 'cart/removeItem';
 
 function reducer(state = initialState, action) {
-  switch (action.type) {
-    case INCREMENT:
-      return { ...state, post: state.post + 1 }
-    case DECREMENT:
-      return { ...state, post: state.post - 1 }
-    case INCREASE_BY:
-      return { ...state, post: state.post + action.payload }
-    case DECREASE_BY:
-      return { ...state, post: state.post - action.payload }
+
+   switch(action.type){
+    case CART_ADD_ITEM:
+      return {...state, cartItems:[...state.cartItems, action.payload]}
+    case CART_REMOVE_ITEM:
+      return {...state, cartItems: state.cartItems.filter(cartItem => cartItem.productId !== action.payload.productId)}
+    case CART_ITEM_INCREASE_QUANTITY:
+      return {...state , cartItems: state.cartItems.map(cartItem => {
+        if(cartItem.productId === action.payload.productId){
+           return {...cartItem, quantity: cartItem.quantity + 1}
+        } 
+        return cartItem;
+      })}  
+
+    case CART_ITEM_DECREASE_QUANTITY:
+      return{...state, cartItems: state.cartItems.map((cartItem) => {
+        if(cartItem.productId === action.payload.productId && cartItem.quantity > 1) {
+          return {...cartItem, quantity: cartItem.quantity - 1}
+        }
+      })}
     default:
-      return state
-  }
+      return state;
+    
+    }
+     
 }
+
+
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
 
-console.log(store)
-
-store.subscribe(() => {
-  console.log(store.getState())
-})
+console.log(store);
 
 
-store.dispatch({ type: INCREMENT })
-store.dispatch({ type: DECREMENT })
-store.dispatch({ type: INCREASE_BY, payload: 15 })
-store.dispatch({ type: DECREASE_BY, payload: 5 })
+
+store.dispatch({type:CART_ADD_ITEM, payload: {productId: 1, quantity: 1}})
+store.dispatch({type:CART_ADD_ITEM, payload: {productId: 12, quantity: 1}})
+store.dispatch({type:CART_ADD_ITEM, payload: {productId: 13, quantity: 1}})
+store.dispatch({type:CART_ADD_ITEM, payload: {productId: 17, quantity: 1}})
+store.dispatch({type:CART_REMOVE_ITEM, payload: {productId: 12}})
+
+store.dispatch({type:CART_ITEM_INCREASE_QUANTITY, payload: {productId: 12}})
+store.dispatch({type:CART_ITEM_INCREASE_QUANTITY, payload: {productId: 12}})
+store.dispatch({type:CART_ITEM_INCREASE_QUANTITY, payload: {productId: 15}})
+store.dispatch({type:CART_ITEM_DECREASE_QUANTITY, payload: {productId: 12}})
+
+
+console.log(store.getState());        
